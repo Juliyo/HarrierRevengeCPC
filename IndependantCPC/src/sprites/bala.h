4853325dd1b2e06c3302b2359f09f7235b26ee81
Toni <tonire.rebollo@gmail.com>
2016-09-29 12:13:19 +0200

    Compilado...

---
+++ IndependantCPC/src/sprites/bala.h
@@ -0,0 +1,13 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2015
+#ifndef _RESOURCES_BALA_H_
+#define _RESOURCES_BALA_H_
+
+#include <types.h>
+#define G_BALA_0_W 2
+#define G_BALA_0_H 4
+extern const u8 g_bala_0[2 * 4];
+#define G_BALA_1_W 2
+#define G_BALA_1_H 1
+extern const u8 g_bala_1[2 * 1];
+
+#endif
