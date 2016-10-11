3437dba96ebf2f413e39b18f2d03a707eaf99b9f
Efeso19 <alfonsojrg19@gmail.com>
2016-10-11 15:32:25 +0200

    subo naves

--- IndependantCPC/src/sprites/naveEnemiga3.h
+++ IndependantCPC/src/sprites/naveEnemiga3.h
@@ -3,17 +3,17 @@
 #define _RESOURCES_NAVEENEMIGA3_H_
 
 #include <types.h>
-#define G_NAVEENEMIGA3_0_W 4
-#define G_NAVEENEMIGA3_0_H 8
-extern const u8 g_naveEnemiga3_0[4 * 8];
-#define G_NAVEENEMIGA3_1_W 4
-#define G_NAVEENEMIGA3_1_H 8
-extern const u8 g_naveEnemiga3_1[4 * 8];
-#define G_NAVEENEMIGA3_2_W 4
-#define G_NAVEENEMIGA3_2_H 8
-extern const u8 g_naveEnemiga3_2[4 * 8];
-#define G_NAVEENEMIGA3_3_W 4
-#define G_NAVEENEMIGA3_3_H 8
-extern const u8 g_naveEnemiga3_3[4 * 8];
+#define G_NAVEENEMIGA3_0_W 5
+#define G_NAVEENEMIGA3_0_H 10
+extern const u8 g_naveEnemiga3_0[5 * 10];
+#define G_NAVEENEMIGA3_1_W 5
+#define G_NAVEENEMIGA3_1_H 10
+extern const u8 g_naveEnemiga3_1[5 * 10];
+#define G_NAVEENEMIGA3_2_W 5
+#define G_NAVEENEMIGA3_2_H 10
+extern const u8 g_naveEnemiga3_2[5 * 10];
+#define G_NAVEENEMIGA3_3_W 5
+#define G_NAVEENEMIGA3_3_H 10
+extern const u8 g_naveEnemiga3_3[5 * 10];
 
 #endif
